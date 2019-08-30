922d00c5445541db934404fdd46d387247761735
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-08-30 14:44:04 -0700

    Delete sort-by-number-of-reviews.js

--- javascripts/amazon/sort-by-number-of-reviews.js
+++
@@ -1,7 +0,0 @@
-function () {
-  var url = new URL(location.href);
-  var search_params = new URLSearchParams(url.search);
-  search_params.set('s', 'review-count-rank');
-  url.search = search_params.toString();
-  location.href = url.toString();
-}
